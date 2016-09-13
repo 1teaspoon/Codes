9a0a6983996a875e20c38c75de0f4e49072f8140
Jeffrey Obadal <Jeffrey-Obadal@UIowa.edu>
2016-09-13 16:40:25 -0500

    added Advanced Pointers

---
+++ L07_Advanced_Pointers/build/CMakeFiles/l07_dynamic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/l07_dynamic.dir/main.cpp.o"
+  "l07_dynamic.pdb"
+  "l07_dynamic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/l07_dynamic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
