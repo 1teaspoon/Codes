a5fd2637c22314759a50e6be8931bfefdc1d5608
1teaspoon <wang.jiahui@icloud.com>
2017-01-31 18:24:17 -0600

    Add files via upload

---
+++ codes/shell scripts/submit_cuff_1L17-.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+/Shared/cufflinks-2.2.1.Linux_x86_64/cufflinks -p 12 -o /Shared/Epigenomeatlas/cufflinks/IL17+ -G /Shared/Epigenomeatlas/gencode.v12.annotation.gtf /Shared/Epigenomeatlas/tophat/IL17+/accepted_hits.bam
